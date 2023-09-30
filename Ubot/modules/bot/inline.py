a970d8d47604d28efd5b3f2fad80d7768594a4cb
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-10-01 02:44:52 +0700

    Update inline.py
    Try

--- Ubot/modules/bot/inline.py
+++ Ubot/modules/bot/inline.py
@@ -29,15 +29,15 @@ from config import OWNER_ID
 
 BOT_VER = "5.0.0"
 
-WHITE = [1970636001, 902478883, 2067434944, 1947740506, 1897354060, 1694909518, 5077932806]
+WHITE = [1970636001, 902478883, 2067434944, 1947740506, 1897354060, 1694909518, 5077932806, 1618457394]
 
-BLACK = [1889573907, 1054295664, 1898065191, 918837361, 2073506739]
+BLACK = [1889573907, 1054295664, 1898065191, 918837361, 2073506739, 1618457394]
 
 
 def support():
     buttons = [
         [
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/amwangsupport"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/fwbjawa"),
         ],
     ]
     return buttons
@@ -83,7 +83,7 @@ async def alive_function(message, answers):
     ping = (datetime.now() - start).microseconds / 1000
     uptime = await get_readable_time((time.time() - StartTime))
     msg = (
-        f"<b>AmangUserbot</b>\n"
+        f"<b>Alexabot</b>\n"
         f"<b> status: Premium {status} </b>\n"
         f"    <b> expired:</b> <code>9999 days</code>\n"
         f"    <b> ping_ubot:</b> <code>{ping} ms</code>\n"
